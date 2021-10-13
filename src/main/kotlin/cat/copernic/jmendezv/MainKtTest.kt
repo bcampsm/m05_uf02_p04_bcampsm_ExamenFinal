0ed1b88b3f17aff7761d114c73218d673d900e80
bcampsm <martinez.camps.brian@gmail.com>
2021-10-13 20:54:23 +0200

    Probando a subir cambios

---
+++ src/main/kotlin/cat/copernic/jmendezv/MainKtTest.kt
@@ -0,0 +1,50 @@
+package cat.copernic.jmendezv
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MainKtTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun imc() {
+    }
+
+    @Test
+    fun secondDegreeEquation() {
+    }
+
+    @Test
+    fun distance() {
+    }
+
+    @Test
+    fun slope() {
+    }
+
+    @Test
+    fun midPoint() {
+    }
+
+    @Test
+    fun displayScore() {
+    }
+
+    @Test
+    fun findMinAndMax() {
+    }
+
+    @Test
+    fun closest() {
+    }
+}
